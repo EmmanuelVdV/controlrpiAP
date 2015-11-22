0954b08f662e7991464dba815aac593f4058b737
EmmanuelVdV <emmanuel.vandevelde@noos.fr>
2015-11-22 21:30:31 +0100

    investigating index.js

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,30 @@ var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  exec("sudo service hostapd status", function (error, stdout, stderr) {
-  	if (error) { // si erreur => page d'erreur
-  		res.render('error', { err : error.message });
-	}
-
-
-	// si le message est "failed", le service est arrêté
-	if (stdout.trim().toLowerCase().indexOf('failed') > -1) { // stdout.trim().toLowerCase().includes('failed')
-		statut=0;
-		res.render('startservice');
-	}
-
-	// sinon le service est up
-	exec("sudo hostapd_cli all_sta | grep \"^[0-9][0-9]:\"", function(error, stdout, stderr) {
-		if (error) {
-			res.render('error', {err : error.message });
+ 	exec("sudo service hostapd status", function (error, stdout, stderr) {
+ 	//DEBUG exec("ls", function (error, stdout, stderr) {
+	  	if (error) { // si erreur => page d'erreur
+	  		res.render('error', { err : error.message });
 		}
 
-		console.log("le message est : "+stdout.trim());
-		var statut=1;
-		var connectedMAC=stdout.trim();
-		res.render('stopservice', {listConnected : connectedMAC });
+
+		// si le message est "failed", le service est arrêté
+		if (stdout.trim().toLowerCase().indexOf('failed') > -1) { // stdout.trim().toLowerCase().includes('failed')
+			statut=0;
+			res.render('startservice');
+		}
+
+		// sinon le service est up
+		exec("sudo hostapd_cli all_sta | grep \"^[0-9][0-9]:\"", function(error1, stdout1, stderr1) {
+		// DEBUG exec("ls", function(error1, stdout1, stderr1) {
+			if (error1) {
+				res.render('error', {err : error1.message });
+			}
+
+			console.log("le message est : "+stdout1.trim());
+			var statut=1;
+			var connectedMAC=stdout1.trim();
+			res.render('stopservice', {listConnected : connectedMAC });
 	});
   });
 });
